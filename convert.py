ab8ae6920ae2182f63b356e3d2890842928ba746
Mason <mason.d.mcmanus@outlook.com>
2023-11-09 21:20:37 -0600

    Fixed corrupted file

--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8s.pt")
+model = YOLO("frc.pt")
 
 model.export(format="onnx", opset=12)
+#results = model(source=0, show=True, conf=0.1, save=True)
